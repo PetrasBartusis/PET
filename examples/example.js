c8df7eae5688ca03d85c891fc6bfc19a4c15f9b6
Unknown <emanuelisc@gmail.com>
2018-05-13 22:51:48 +0300

    800 eiluciu

--- examples/example.js
+++ examples/example.js
@@ -1,19 +1,17 @@
 function sum(num) {
     console.log(num);
-    ;
-    if (num < 5) {
+    if (num < 5 & num > 3) {
         return sum(num + 1);
     } else {
         return 6;
     }
 }
 var p = sum(0);
+p = Math.abs(p);
 console.log(p);
-;
 console.log('CIKLAS:');
-for (var a = 0; a < 10; a++) {
+for (var a = 0; a < 10; a = a + 1) {
     var something = a * a;
     console.log(something);
-    ;
 }
 console.log('a');
